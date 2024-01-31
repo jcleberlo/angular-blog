d0e8eebf124acd9cd82c3e12f0fc45f7373743d5
jcleberlo <jcleberlo@hotmail.com>
2024-01-31 14:34:58 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SmallCardComponent implements OnInit {
   @Input() photoCover: string = '';
   @Input() cardTitle: string = '';
+  @Input() id = '0';
+
   constructor() {}
 
   ngOnInit(): void {}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Saiu a nova versão do Angular',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rematque libero saepe aliquam provident consectetur voluptate natusblanditiis dolore, pariatur inventore ipsum sed, fuga officiis delectus.Iusto, totam ipsa.',
+    photoCover: 'http://cansunmanpower.com/assets/images/imagePlaceholder.png',
+  },
+  {
+    id: '2',
+    title: 'Saiu a nova versão do Angular',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rematque libero saepe aliquam provident consectetur voluptate natusblanditiis dolore, pariatur inventore ipsum sed, fuga officiis delectus.Iusto, totam ipsa.',
+    photoCover: 'http://cansunmanpower.com/assets/images/imagePlaceholder.png',
+  },
+  {
+    id: '3',
+    title: 'Saiu a nova versão do Angular',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rematque libero saepe aliquam provident consectetur voluptate natusblanditiis dolore, pariatur inventore ipsum sed, fuga officiis delectus.Iusto, totam ipsa.',
+    photoCover: 'http://cansunmanpower.com/assets/images/imagePlaceholder.png',
+  },
+  {
+    id: '4',
+    title: 'Saiu a nova versão do Angular',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rematque libero saepe aliquam provident consectetur voluptate natusblanditiis dolore, pariatur inventore ipsum sed, fuga officiis delectus.Iusto, totam ipsa.',
+    photoCover: 'http://cansunmanpower.com/assets/images/imagePlaceholder.png',
+  },
+];
